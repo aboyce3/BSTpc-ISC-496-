40fa2c7b3c79c18a7a6af5bf3d7b5c528b07f067
Andrew Boyce <andyboyce30@gmail.com>
2020-10-03 15:30:51 -0400

    Update User.java

--- BSTpc/src/main/java/User.java
+++ BSTpc/src/main/java/User.java
@@ -21,22 +21,22 @@ public class User {
         wishlist = new WishList(user);
     }
 
-    String getfirstName(){return firstName;}
-    String getlastName(){return lastName;}
-    String getemail(){return email;}
-    String getuserName(){return userName;}
+    String getFirstName(){return firstName;}
+    String getLastName(){return lastName;}
+    String getEmail(){return email;}
+    String getUserName(){return userName;}
     String getIMG(){return img_Location;}
 
-    void setfirstName(String First){firstName = First;}
-    void setlastName(String Last){lastName = Last;}
-    void setemail(String email){this.email = email;}
+    void setFirstName(String First){firstName = First;}
+    void setLastName(String Last){lastName = Last;}
+    void setEmail(String email){this.email = email;}
     void setIMG(String IMG){img_Location = IMG;}
-    void setuserName(String userName){this.userName = userName;}
+    void setUserName(String userName){this.userName = userName;}
     void setWishList(WishList wishlist) {this.wishlist = wishlist;}
-    void setAuctionedlist(Auctioned auctioned) {this.userAuctionedItems = auctioned;}
+    void setAuctionedList(Auctioned auctioned) {this.userAuctionedItems = auctioned;}
     void setShoppingCart(ShoppingCart cart) {this.cart = cart;}
 
     void addToCart(Part item){cart.add(item);}
-    void addToAuctionedlist(Part item){userAuctionedItems.add(item);}
+    void addToAuctionedList(Part item){userAuctionedItems.add(item);}
     void addToWishlist(Part item){wishlist.add(item);}
 }
